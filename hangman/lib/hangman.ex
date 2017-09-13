f527f827ce6b6b09c4c4bb998a0f056d9fd9eaff
pragdave <dave@pragdave.me>
2017-09-13 16:43:28 +0800

    add human player

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,5 +3,7 @@ defmodule Hangman do
   defdelegate new_game(),             to: Hangman.Game
   
   defdelegate make_move(game, guess), to: Hangman.Game
-  
+
+  defdelegate tally(game),            to: Hangman.Game
+
 end
--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -5,6 +5,7 @@ defmodule Hangman.Game do
     game_state: :initializing,
     letters:    [],
     used:       MapSet.new(),
+    last_guess: "",
   )
 
   def new_game(word) do
@@ -17,13 +18,16 @@ defmodule Hangman.Game do
     new_game(Dictionary.random_word)
   end
 
-  def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
+  def make_move(game = %{ game_state: state }, _guess)
+  when state in [:won, :lost] do
     game
     |> return_with_tally()
   end
 
   def make_move(game, guess) do
-    accept_move(game, guess, MapSet.member?(game.used, guess))
+    game
+    |> Map.put(:last_guess, guess)
+    |> accept_move(MapSet.member?(game.used, guess))
     |> return_with_tally()
   end
 
@@ -32,7 +36,8 @@ defmodule Hangman.Game do
       game_state: game.game_state,
       turns_left: game.turns_left,
       letters:    game.letters |> reveal_guessed(game.used),
-      used:       game.used    |> MapSet.to_list |> Enum.sort()
+      used:       game.used    |> MapSet.to_list |> Enum.sort(),
+      last_guess: game.last_guess,
     }
   end
 
@@ -40,7 +45,7 @@ defmodule Hangman.Game do
   ############################################################
   ## your code goes here...
   
-  defp accept_move(game, guess, already_guessed) do
+  defp accept_move(game, already_guessed) do
      # ...
   end
 

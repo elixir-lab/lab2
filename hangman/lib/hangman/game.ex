dadedd3b609d4803907aa9ca47b3b59efabd1017
pragdave <dave@pragdave.me>
2018-11-14 11:13:10 -0600

    update for elixir 1.8

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -5,7 +5,7 @@ defmodule Hangman.Game do
     game_state: :initializing,
     letters:    [],
     used:       MapSet.new(),
-    last_guess: "",
+    last_guess: ""
   )
 
   def new_game(word) do
@@ -13,7 +13,7 @@ defmodule Hangman.Game do
       letters: word |> String.codepoints
     }
   end
-  
+
   def new_game() do
     new_game(Dictionary.random_word)
   end
@@ -43,10 +43,33 @@ defmodule Hangman.Game do
 
 
   ############################################################
-  ## your code goes here...
-  
-  defp accept_move(game, already_guessed) do
-     # ...
+  #
+  # We're passed the game state, which has already been
+  # updated to contain the letter just guessed (in `last_guess`). We
+  # also are passed a flag which indicates whether that particular
+  # letter has already been used.
+  #
+  # We return an updated state:
+  #
+  # * `game_state` will be one of :good_guess, :bad_guess, #
+  #   :already_used, :won, or :lost
+  #
+  # * the `used` MapSet will include the letter just guessed
+  #
+  # * the number of turns left will have been decremented if the guess
+  #   was bad.
+  #
+  # Things that might be useful:
+  #
+  # `Map.put` adds a key/value pair to a map or struct
+  # `MapSet.out` adds an entry to a set
+  # `MapSet.new(list)` creates a new set containing those elements
+  # `MapSet.subset(a, b)` returns true if `a` is a subset of `b`
+  #
+  # I suggest making one test at a time pass.
+
+  defp accept_move(game, already_guessed?) do
+    ## your code goes here...
   end
 
   # other functions you write
@@ -54,7 +77,7 @@ defmodule Hangman.Game do
 
   # and some helpers I wrote...
 
-  
+
   defp reveal_guessed(letters, used) do
     letters
     |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
@@ -63,7 +86,7 @@ defmodule Hangman.Game do
   defp reveal_letter(letter, _in_word = true),  do: letter
   defp reveal_letter(_letter, _not_in_word),    do: "_"
 
-  
+
   defp return_with_tally(game), do: { game, tally(game) }
-  
+
 end
